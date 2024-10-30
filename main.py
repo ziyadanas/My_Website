350fcf83a7906a4c00abe3d56aeccb18c9655258
ziyadanas <ziyadanas125@gmail.com>
2024-10-31 07:11:07 +0800

    update code

--- main.py
+++ main.py
@@ -15,4 +15,5 @@ def financePlanner():
     return render_template('finance_planner.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
